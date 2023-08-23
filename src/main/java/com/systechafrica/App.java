1bcd49768b1dae281e052dbe5b0c9dad26fb4c83
Immaculatekimani <immaculatewanjira21@gmail.com>
2023-08-23 16:05:55 +0300

    use logger instead

--- src/main/java/com/systechafrica/App.java
+++ src/main/java/com/systechafrica/App.java
@@ -1,11 +1,13 @@
 package com.systechafrica;
 
+import java.util.logging.Logger;
 
 public class App 
 {
+    private static final Logger LOGGER = Logger.getLogger(App.class.getName());
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        LOGGER.info( "Hello World!" );
     }
     
     public int add(int a, int b){
