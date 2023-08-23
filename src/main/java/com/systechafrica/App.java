9dbdab8fc2ec54ed6fa68eecbd44be45b8950ee4
Immaculatekimani <immaculatewanjira21@gmail.com>
2023-08-23 13:25:48 +0300

    my first commit

---
+++ src/main/java/com/systechafrica/App.java
@@ -0,0 +1,13 @@
+package com.systechafrica;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
