2e499a110eb24e52c572d2d932d8603d5fcbe8c8
Immaculatekimani <immaculatewanjira21@gmail.com>
2023-08-25 14:47:23 +0300

    Done grading statements

--- src/main/java/com/systechafrica/controlflow/ControlFlow.java
+++ src/main/java/com/systechafrica/controlflow/ControlFlow.java
@@ -6,15 +6,26 @@ public class ControlFlow {
     private static final Logger LOGGER = Logger.getLogger(ControlFlow.class.getName());
 
     public void ifelseStatement() {
-        int maths = 70;
+        int maths = 76;
         char grade;
 
         if (maths >= 70) {
             grade = 'A';
+        }else if (maths >= 60){
+            grade = 'B';
+        }else if (maths >= 50){
+            grade = 'C';
+        }else if (maths >= 40){
+            grade = 'D';
+        }else if (maths >= 30){
+            grade = 'E';
+        }else{
+            grade = 'F';
         }
+
         // TODO: add conditions here to assign grade variable as expected
-        LOGGER.info("Student grade: "); // + grade
-    }
+        LOGGER.info("Student grade: " + grade); // + grade
+    } 
     public void switchStatement(){
         String day = "Unknown";
         switch(day){
@@ -47,8 +58,8 @@ public class ControlFlow {
     }
     public static void main(String[] args) {
         ControlFlow app = new ControlFlow();
-      //  app.ifelseStatement();
-        app.switchStatement();
+        app.ifelseStatement();
+        //app.switchStatement();
 
     }
 }
