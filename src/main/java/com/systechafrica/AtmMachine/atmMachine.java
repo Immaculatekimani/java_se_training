d8dc5643cb778b7766daaeae23771b0ae3f23669
Immaculatekimani <immaculatewanjira21@gmail.com>
2023-08-30 12:35:09 +0300

    Made a main menu display

--- src/main/java/com/systechafrica/AtmMachine/atmMachine.java
+++ src/main/java/com/systechafrica/AtmMachine/atmMachine.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class atmMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        atmMachine app = new atmMachine();
+
         final String DB_PASSWORD = "Admin123";
         final double INITIAL_BAL = 1000.00;
         int trials = 0;
@@ -18,7 +20,7 @@ public class atmMachine {
             System.out.print("Please enter your password: ");
             userPassword = scanner.nextLine();
             if(userPassword.equals(DB_PASSWORD)){
-                System.out.println("Welcome to Tatu's Atm!");
+                app.atmMenu();
                 break;
             }else{
                 trials++;
@@ -33,7 +35,25 @@ public class atmMachine {
             
         }
         
-
+        scanner.close();
+    }
+    public void atmMenu(){
+        System.out.println("***************************************************************");
+        System.out.println();
+        System.out.println("                       ATM SIMULATOR                           ");
+        System.out.println();
+        System.out.println("***************************************************************");
+        System.out.println();
+        System.out.println("                       ATM SERVICES                            ");
+        System.out.println("_______________________________________________________________");
+        System.out.println();
+        System.out.println("1. Check Balance");
+        System.out.println("2. Deposit");
+        System.out.println("3. Withdraw");
+        System.out.println("4. Transfer Cash");
+        System.out.println("5. Quit");
+        System.out.println("***************************************************************");
+        System.out.println("Choose your option: ");
     }
     
 }
