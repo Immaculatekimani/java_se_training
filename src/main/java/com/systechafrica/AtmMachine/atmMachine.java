73188bb86a23b342bb610bf04a2819ef2e6a5b18
Immaculatekimani <immaculatewanjira21@gmail.com>
2023-08-30 14:17:35 +0300

    Accept options and quiting the program

--- src/main/java/com/systechafrica/AtmMachine/atmMachine.java
+++ src/main/java/com/systechafrica/AtmMachine/atmMachine.java
@@ -13,47 +13,71 @@ public class atmMachine {
         String userPassword;
         String userName;
 
-        
-        while(trials < 3){
+        while (trials < 3) {
             System.out.print("Please enter you username: ");
             userName = scanner.nextLine();
             System.out.print("Please enter your password: ");
             userPassword = scanner.nextLine();
-            if(userPassword.equals(DB_PASSWORD)){
-                app.atmMenu();
+            if (userPassword.equals(DB_PASSWORD)) {
+                app.atmMenu(scanner);
                 break;
-            }else{
+            } else {
                 trials++;
-                if (trials < 3){
-                    System.out.println("Wrong username or password," );
-                }else{
+                if (trials < 3) {
+                    System.out.println("Wrong username or password,");
+                } else {
                     System.out.println("You have exhausted your login attempts");
                 }
-                
+
             }
-            
-            
+
         }
-        
+
         scanner.close();
     }
-    public void atmMenu(){
-        System.out.println("***************************************************************");
-        System.out.println();
-        System.out.println("                       ATM SIMULATOR                           ");
-        System.out.println();
-        System.out.println("***************************************************************");
-        System.out.println();
-        System.out.println("                       ATM SERVICES                            ");
-        System.out.println("_______________________________________________________________");
-        System.out.println();
-        System.out.println("1. Check Balance");
-        System.out.println("2. Deposit");
-        System.out.println("3. Withdraw");
-        System.out.println("4. Transfer Cash");
-        System.out.println("5. Quit");
-        System.out.println("***************************************************************");
-        System.out.println("Choose your option: ");
+
+    public void atmMenu(Scanner scanner) {
+        while (true) {
+            // the main display area
+            System.out.println("***************************************************************");
+            System.out.println();
+            System.out.println("                       ATM SIMULATOR                           ");
+            System.out.println();
+            System.out.println("***************************************************************");
+            System.out.println();
+            System.out.println("                       ATM SERVICES                            ");
+            System.out.println("_______________________________________________________________");
+            System.out.println();
+            System.out.println("1. Check Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Transfer Cash");
+            System.out.println("5. Quit");
+            System.out.println("***************************************************************");
+            System.out.println("Choose your option: ");
+            // Dealing with options
+            int option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("YOu are checking balance");
+                    break;
+                case 2:
+                    System.out.println("Make a deposit");
+                    break;
+                case 3:
+                    System.out.println("Make a withdraw");
+                    break;
+                case 4:
+                    System.out.println("Make a transfer");
+                    break;
+                case 5:
+                    System.out.println("Sad to let you go");
+                    return;
+                default:
+                    System.out.println("Please enter a valid option");
+            }
+        }
+
     }
-    
+
 }
