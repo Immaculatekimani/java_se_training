5c7c07e923069c3289d4a209687bea98b0696a24
Immaculatekimani <immaculatewanjira21@gmail.com>
2023-09-07 21:27:59 +0300

    created the order class

--- src/main/java/com/systechafrica/restaurantpos/MenuItem.java
+++ src/main/java/com/systechafrica/restaurantpos/MenuItem.java
@@ -5,9 +5,8 @@ public class MenuItem {
     private String itemName;
     private double itemPrice;
 
-    public void MenuItem (int itemNO, String itemName, double itemPrice){
-        this.itemNumber = itemNO;
-        this.itemName = itemName;
+    public void MenuItem (int itemNumber, String itemName, double itemPrice){
+        this.itemNumber = itemNumber;
         this.itemPrice = itemPrice;
     }
     public void MenuItem(){
---
+++ src/main/java/com/systechafrica/restaurantpos/Order.java
@@ -0,0 +1,37 @@
+package com.systechafrica.restaurantpos;
+
+public class Order {
+    
+    private double totalAmount = 0.0;
+    final int MAX_ITEMS = 1000;
+    private MenuItem[] items = new MenuItem[MAX_ITEMS];
+
+    public void addItem(MenuItem item){
+        for (int index = 0; index < items.length; index++) {
+            if(items[index] == null){
+                items[index] = item;
+                totalAmount += item.getItemPrice();
+            }
+        }
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+    public int getMAX_ITEMS() {
+        return MAX_ITEMS;
+    }
+    public MenuItem[] getItems() {
+        return items;
+    }
+    public void setItems(MenuItem[] items) {
+        this.items = items;
+    }
+
+    
+    
+    
+}
